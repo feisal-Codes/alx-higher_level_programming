8acfbfef758b30089861c2aa0a50d8ca5f491026
Feisal Mohamed <feisalmib@gmail.com>
2022-08-12 23:12:37 +0300

    Update 4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -2,4 +2,4 @@
 -- If the table first_table already exists, your script should not fail
 -- You are not allowed to use the SELECT or SHOW statements
 -- script creates table called first_table with id(INT) and name VARCHAR(256)
-CREATE TABLE IF NOT EXISTS first_table (sd INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
